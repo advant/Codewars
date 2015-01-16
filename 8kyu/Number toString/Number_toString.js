3bc78e22a89d28d6d104ac504078747c0fdfbf15
Drew Hoang <drewhoang@Drews-MacBook-Pro.local>
2015-01-15 23:38:33 -0800

    add Number toString

---
+++ 8kyu/Number toString/Number_toString.js
@@ -0,0 +1,8 @@
+// Description:
+// The code gives an error!
+// a = 123.toString
+// var a = 123.toString
+var a = 123.toString();
+var a = 123 . toString();
+var a = (123).toString();
+var a = 123 + '';
